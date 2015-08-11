b86a66f797bd8f5f1f259df3654c148d2580d9a2
Ruofan Kong <krf1088@gmail.com>
2015-08-11 13:09:38 -0400

    update upload_datat_s3.py

--- upload_data_s3.py
+++ upload_data_s3.py
@@ -7,7 +7,7 @@ from boto.s3.connection import Location
 def main():
 	### Connect to S3.
 	s3 = boto.connect_s3()
-	bucket = s3.get_bucket("synapsify-ruofan")	### Put the bucket name here.
+	bucket = s3.get_bucket("synapsify-lstm")	### Put the bucket name here.
 
 	### Current directory path, and specify the data directory.
 	curr_dir = os.path.split(inspect.getfile(inspect.currentframe()))[0]
@@ -18,7 +18,7 @@ def main():
 		# sourcePath = os.path.join(sourceDir, filename)
 		# destPath = os.path.join("data/", filename)
 		sourcePath = os.path.join(data_dir, ff)
-		destPath = os.path.join("Synapsify_data/", ff)
+		destPath = os.path.join("Synapsify_data1/", ff)
 		k = boto.s3.key.Key(bucket)
 		k.key = destPath
 		k.set_contents_from_filename(sourcePath)
